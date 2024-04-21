f854da8687da30eaf142bd611ef4c42518b23c2b
Ianislava Chalbysheva <ya.chalbysheva@gmail.com>
2024-04-21 15:50:02 +0700

    Initial Commit

---
+++ cardgame/cardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardgameApp.swift
+//  cardgame
+//
+//  Created by Янислава Чалбышева on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct cardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
